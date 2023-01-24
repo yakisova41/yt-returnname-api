ff59f1a5c5a2b35156fe9ca75d5af854a2617d23
yakisova41 <yakisoba5emon@gmail.com>
2023-01-24 18:44:28 +0000

    fuckcors

--- pages/api/idToName/[id].js
+++ pages/api/idToName/[id].js
@@ -1,7 +1,30 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { Innertube } from 'youtubei.js';
+import Cors from 'cors'
+
+const cors = Cors({
+    methods: ['POST', 'GET', 'HEAD'],
+})
+
+function runMiddleware(
+    req,
+    res,
+    fn
+) {
+    return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+        if (result instanceof Error) {
+        return reject(result)
+        }
+
+        return resolve(result)
+    })
+    })
+}
 
 export default async function handler(req, res) {
+    await runMiddleware(req, res, cors)
+    
     const { id } = req.query
     const youtube = await Innertube.create();
     youtube.getChannel(id)
